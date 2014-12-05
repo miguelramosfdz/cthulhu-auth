c812eb52e4ed643ff1a808bb4ee8fe268b95c0ec
CJ Ponti <cjponti@gmail.com>
2014-12-05 14:40:51 +0000

    Add test files

---
+++ test/auth-spec.js
@@ -0,0 +1,63 @@
+describe("Util: auth", function() {
+  "use strict";
+
+  var req, auth, next;
+  var http = require('http');
+
+  beforeEach(function() {
+    next = jasmine.createSpy('next');
+    auth = require('../src/auth');
+    req = http.IncomingMessage;
+    req.session = {};
+  });
+
+  afterEach(function() {
+    auth = null;
+    req = null;
+  });
+
+  describe("#isAuthenticated", function() {
+    it("should return true if user", function() {
+      req.user = true;
+      expect(auth.isAuthenticated.apply(req)).toEqual(true);
+    });
+    it("should return false if no user", function() {
+      req.user = false;
+      expect(auth.isAuthenticated.apply(req)).toEqual(false);
+    });
+  });
+
+  describe("#logIn", function() {
+    it("should set value of req.session", function() {
+      auth.logIn.call(req, { id: "foo" });
+      expect(req.session.user).toEqual("foo");
+    });
+  });
+
+  describe(".deserializeUser()", function() {
+
+    var callback = jasmine.createSpy('callback');
+
+    it("should call next if no req.session.user", function() {
+      req.session.user = undefined;
+      auth.deserializeUser(callback)(req, undefined, next);
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should call callback if req.session.user', function() {
+      req.session.user = '1234';
+      auth.deserializeUser(callback)(req, undefined, next);
+      expect(callback).toHaveBeenCalled();
+      expect(next).not.toHaveBeenCalled();
+    });
+
+  });
+
+  describe(".logOut()", function() {
+    it("should set value of req.session", function() {
+      auth.logOut.call(req);
+      expect(req.session.user).toEqual(undefined);
+    });
+  });
+
+});
--- test/oauth-spec.js
+++ test/oauth-spec.js
@@ -1,190 +1,189 @@
-describe('Util: OAuth', function() {
-  'use strict';
-  
-  var oauth = require('../../util/oauth');
+'use strict';
 
-  console.log(oauth);
+describe('Util: OAuth', function() {
+
+  var oauth = require('../src/index');
 
   describe('Facebook Strategy', function() {
     it('should throw error if no app_id', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Facebook: {
           app_secret: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Facebook with app_id');
+        }
+      })).toThrow('Must supply Facebook with app_id');
     });
     it('should throw error if no app_secret', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Facebook: {
           app_id: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Facebook with app_secret');
+        }
+      })).toThrow('Must supply Facebook with app_secret');
     });
     it('should throw error if no callback_url', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Facebook: {
           app_id: 'fooId',
           app_secret: 'fooSecret'
-        } 
-      })).to.throw('Must supply Facebook with callback_url');
+        }
+      })).toThrow('Must supply Facebook with callback_url');
     });
     it('should set Facebook', function() {
-      var auth = oauth({ 
+      var auth = oauth({
         Facebook: {
           app_id: 'fooId',
           app_secret: 'fooSecret',
           callback_url: 'http://foo.com'
-        } 
+        }
       });
-      expect(auth.Facebook.app_id).to.equal('fooId');
-      expect(auth.Facebook.app_secret).to.equal('fooSecret');
-      expect(auth.Facebook.callback_url).to.equal('http://foo.com');
+      expect(auth.Facebook.app_id).toEqual('fooId');
+      expect(auth.Facebook.app_secret).toEqual('fooSecret');
+      expect(auth.Facebook.callback_url).toEqual('http://foo.com');
     });
   });
 
   describe('Google Strategy', function() {
     it('should throw error if no client_id', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Google: {
           app_secret: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Google Strategy with client_id');
+        }
+      })).toThrow('Must supply Google Strategy with client_id');
     });
     it('should throw error if no client_secret', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Google: {
           client_id: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Google Strategy with client_secret');
+        }
+      })).toThrow('Must supply Google Strategy with client_secret');
     });
     it('should throw error if no redirect_uri', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Google: {
           client_id: 'fooId',
           client_secret: 'fooSecret'
-        } 
-      })).to.throw('Must supply Google Strategy with redirect_uri');
+        }
+      })).toThrow('Must supply Google Strategy with redirect_uri');
     });
     it('should set Google', function() {
-      var auth = oauth({ 
+      var auth = oauth({
         Google: {
           client_id: 'fooId',
           client_secret: 'fooSecret',
           redirect_uri: 'http://foo.com'
-        } 
+        }
       });
-      expect(auth.Google.client_id).to.equal('fooId');
-      expect(auth.Google.client_secret).to.equal('fooSecret');
-      expect(auth.Google.redirect_uri).to.equal('http://foo.com');
+      expect(auth.Google.client_id).toEqual('fooId');
+      expect(auth.Google.client_secret).toEqual('fooSecret');
+      expect(auth.Google.redirect_uri).toEqual('http://foo.com');
     });
   });
 
   describe('Twitter Strategy', function() {
     it('should throw error if no client_id', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Twitter: {
           consumer_secret: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Twitter Strategy with consumer_key');
+        }
+      })).toThrow('Must supply Twitter Strategy with consumer_key');
     });
     it('should throw error if no client_secret', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Twitter: {
           consumer_key: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Twitter Strategy with consumer_secret');
+        }
+      })).toThrow('Must supply Twitter Strategy with consumer_secret');
     });
     it('should throw error if no callback_url', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Twitter: {
           consumer_key: 'fooId',
           consumer_secret: 'fooSecret'
-        } 
-      })).to.throw('Must supply Twitter Strategy with callback_url');
+        }
+      })).toThrow('Must supply Twitter Strategy with callback_url');
     });
     it('should set Twitter', function() {
-      var auth = oauth({ 
+      var auth = oauth({
         Twitter: {
           consumer_key: 'fooId',
           consumer_secret: 'fooSecret',
           callback_url: 'http://foo.com'
-        } 
+        }
       });
-      expect(auth.Twitter.consumer_key).to.equal('fooId');
-      expect(auth.Twitter.consumer_secret).to.equal('fooSecret');
-      expect(auth.Twitter.callback_url).to.equal('http://foo.com');
+      expect(auth.Twitter.consumer_key).toEqual('fooId');
+      expect(auth.Twitter.consumer_secret).toEqual('fooSecret');
+      expect(auth.Twitter.callback_url).toEqual('http://foo.com');
     });
   });
 
   describe('Foursquare Strategy', function() {
     it('should throw error if no client_id', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Foursquare: {
           client_secret: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Foursquare Strategy with client_id');
+        }
+      })).toThrow('Must supply Foursquare Strategy with client_id');
     });
     it('should throw error if no client_secret', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Foursquare: {
           client_id: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Foursquare Strategy with client_secret');
+        }
+      })).toThrow('Must supply Foursquare Strategy with client_secret');
     });
     it('should throw error if no callback_url', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Foursquare: {
           client_id: 'fooId',
           client_secret: 'fooSecret'
-        } 
-      })).to.throw('Must supply Foursquare Strategy with callback_url');
+        }
+      })).toThrow('Must supply Foursquare Strategy with callback_url');
     });
     it('should set Foursquare', function() {
-      var auth = oauth({ 
+      var auth = oauth({
         Foursquare: {
           client_id: 'fooId',
           client_secret: 'fooSecret',
           callback_url: 'http://foo.com'
-        } 
+        }
       });
-      expect(auth.Foursquare.client_id).to.equal('fooId');
-      expect(auth.Foursquare.client_secret).to.equal('fooSecret');
-      expect(auth.Foursquare.callback_url).to.equal('http://foo.com');
+      expect(auth.Foursquare.client_id).toEqual('fooId');
+      expect(auth.Foursquare.client_secret).toEqual('fooSecret');
+      expect(auth.Foursquare.callback_url).toEqual('http://foo.com');
     });
   });
 
   describe('Github Strategy', function() {
     it('should throw error if no client_id', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Github: {
           client_secret: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Github Strategy with client_id');
+        }
+      })).toThrow('Must supply Github Strategy with client_id');
     });
     it('should throw error if no client_secret', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Github: {
           client_id: 'fooId',
           callback_url: 'http://foo.com'
-        } 
-      })).to.throw('Must supply Github Strategy with client_secret');
+        }
+      })).toThrow('Must supply Github Strategy with client_secret');
     });
     it('should throw error if no callback_url', function() {
-      expect(oauth.bind(this, { 
+      expect(oauth.bind(this, {
         Github: {
           client_id: 'fooId',
           client_secret: 'fooSecret'
-        } 
-      })).to.throw('Must supply Github Strategy with callback_url');
+        }
+      })).toThrow('Must supply Github Strategy with callback_url');
     });
     it('should set Github', function() {
       var auth = oauth({
@@ -192,11 +191,11 @@ describe('Util: OAuth', function() {
           client_id: 'fooId',
           client_secret: 'fooSecret',
           callback_url: 'http://foo.com'
-        } 
+        }
       });
-      expect(auth.Github.client_id).to.equal('fooId');
-      expect(auth.Github.client_secret).to.equal('fooSecret');
-      expect(auth.Github.callback_url).to.equal('http://foo.com');
+      expect(auth.Github.client_id).toEqual('fooId');
+      expect(auth.Github.client_secret).toEqual('fooSecret');
+      expect(auth.Github.callback_url).toEqual('http://foo.com');
     });
   });
 
