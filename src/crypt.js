04fc481ac33a7ff22ae98709ea746a1d11d52819
CJ Ponti <cjponti@gmail.com>
2014-12-05 14:40:23 +0000

    Refactor crypt.js

--- src/crypt.js
+++ src/crypt.js
@@ -3,65 +3,58 @@
 var crypto = require('crypto');
 
 /**
- * `Crypt` constructor
- * @constructor
+ * Bytesize.
+ * @type {number}
+ * @private
  */
-var Crypt = function() {
-  var self = this;
+var len = 128;
 
-  /**
-   * Bytesize.
-   */
-  var len = 128;
+/**
+ * Iterations. ~300ms
+ * @type {number}
+ * @private
+ */
+var iterations = 12000;
 
-  /**
-   * Iterations. ~300ms
-   */
-  var iterations = 12000;
-  
-  /**
-   * @desc Make salt
-   * @return {String}
-   * @api public
-   */
-  self.makeSalt = function() {
-    return crypto.randomBytes(16).toString('base64');
-  };
-
-  /**
-   * Hashes a string with optional `salt`, otherwise
-   * generate a salt for `pass` and invoke `fn(err, salt, hash)`.
-   * @param {String} string String to hash
-   * @param {String} optional salt
-   * @param {Function} callback
-   * @api public
-   */
-  self.hash = function(string, salt, callback) {
-    if (salt) {
-      crypto.pbkdf2(string, salt, iterations, len, callback);
-    } else {
-      salt = self.makeSalt();
-      crypto.pbkdf2(string, salt, iterations, len, function(err, hash) {
-        if (err) {
-          return callback(err);
-        }
-        callback(null, salt, hash);
-      });
-    }
-  };
-
-  /**
-   * Create HMAC from text based on APP_KEY
-   * @return {String}
-   */
-  self.makeHMAC = function(text) {
-    var hmac = crypto.createHmac('sha1', process.env.APP_KEY);
-    hmac.setEncoding('hex');
-    hmac.write(text);
-    hmac.end();
-    return hmac.read();
-  };
-  
+/**
+ * Make 16 byte, base 24 string salt
+ * @return {String}
+ * @public
+ */
+var makeSalt =
+exports.makeSalt = function() {
+  return crypto.randomBytes(16).toString('base64');
 };
 
-module.exports = new Crypt();
+/**
+ * Hashes a string with optional `salt`, otherwise generate a salt for `pass`
+ * and invoke `fn(err, salt, hash)`.
+ * @param {String} string String to hash
+ * @param {String} optional salt
+ * @param {Function} callback
+ * @public
+ */
+exports.hash = function(string, salt, callback) {
+  salt = salt || makeSalt();
+  crypto.pbkdf2(string, salt, iterations, len, function(err, hash) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, salt, hash);
+  });
+};
+
+/**
+ * Create HMAC from text based on APP_KEY
+ * @param {string} text Text to create HMAC
+ * @param {string} key Key to use for creating HMAC
+ * @return {String}
+ * @public
+ */
+exports.makeHMAC = function(text, key) {
+  var hmac = crypto.createHmac('sha1', key);
+  hmac.setEncoding('hex');
+  hmac.write(text);
+  hmac.end();
+  return hmac.read();
+};
